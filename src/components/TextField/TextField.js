520168c11da30b4cd225ecbc89a37f0b48f33b92
Juan <juan.lopez15@utp.edu.co>
2024-11-28 11:40:38 -0500

    Add setValue in TextField

--- src/components/TextField/TextField.js
+++ src/components/TextField/TextField.js
@@ -1,9 +1,18 @@
-import './TextField.css'
-const TextField = ({ label, placeholder, required }) => {
+import "./TextField.css";
+const TextField = ({ label, placeholder, value, setValue, required }) => {
+    const handleChange = (event) => {
+        setValue(event.target.value);
+    };
+
     return (
-        <div className='textfield'>
+        <div className="textfield">
             <label>{label}</label>
-            <input placeholder={placeholder} required={required} />
+            <input
+                placeholder={placeholder}
+                required={required}
+                value={value}
+                onChange={handleChange}
+            />
         </div>
     );
 };
